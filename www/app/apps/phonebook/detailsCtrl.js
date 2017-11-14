a297f9f147c7e363813fbadc0661e41999dd7e3d
Golan Hadad <ghadad@gmail.com>
2017-11-14 11:39:34 +0200

    WIP:phonebook

--- www/app/apps/phonebook/detailsCtrl.js
+++ www/app/apps/phonebook/detailsCtrl.js
@@ -23,13 +23,34 @@ angular.module('pele')
         return "data:image/jpg;" + base64String;
       }
 
+      $scope.getTreeData = function(person) {
+        var tree = {};
+        person.membersTree.forEach(function(c) {
+
+          if (c.personId == person.directManagerNumber) {
+            return false;
+          }
+          tree[c.directManagerNumber] = tree[c.directManagerNumber] || {};
+          tree[c.directManagerNumber].members = tree[c.directManagerNumber].members || [];
+
+          if (person.personId == c.directManagerNumber)
+            tree[person.personId].members.push(c)
+          else
+            tree[c.personId] = c;
+        })
+        return tree;
+
+      }
+
+
       $scope.getContact = function() {
         PelApi.getLocalJson("mocks/phonebook_details.json")
           .success((data, status, headers, config) => {
             console.log(JSON.stringify(data))
             $scope.contact = data;
-
             $scope.title = "פרטי עובד : " + data.lastName + " " + data.firstName;
+            $scope.contact.tree = $scope.getTreeData(data);
+
 
           })
           .error((errorStr, httpStatus, headers, config) => {})
