ec5ff209b31e74d62bb39869fbb46abef695cbdd
Golan Hadad <ghadad@gmail.com>
2017-11-19 15:33:16 +0200

    WIP:phonebook

--- www/app/apps/phonebook/detailsCtrl.js
+++ www/app/apps/phonebook/detailsCtrl.js
@@ -5,74 +5,75 @@ angular.module('pele', ['ngCordova'])
   //=================================================================
   //==                    PAGE_4
   //=================================================================
-  .controller('phonebookDetailsCtrl', ['$scope', '$stateParams', '$ionicLoading', '$ionicModal', 'PelApi', '$ionicHistory', '$ionicPopup', '$cordovaSocialSharing',
-    function($scope, $stateParams, $ionicLoading, $ionicModal, PelApi, $ionicHistory, $ionicPopup, $cordovaSocialSharing) {
+  .controller('phonebookDetailsCtrl', ['$scope', '$stateParams', '$ionicLoading', '$ionicModal', 'PelApi', '$ionicHistory', '$ionicPopup', '$cordovaSocialSharing', '$cordovaContacts',
+    function($scope, $stateParams, $ionicLoading, $ionicModal, PelApi, $ionicHistory, $ionicPopup, $cordovaSocialSharing, $cordovaContacts) {
 
       //$scope.shareViaWhatsAppToReceiver = $cordovaSocialSharing.shareViaWhatsAppToReceiver;
       //$scope.shareViaEmail = $cordovaSocialSharing.shareViaEmail;
       //$scope.shareViaSMS = $cordovaSocialSharing.shareViaSMS;
+      //$cordovaContacts.find().then(function(allContacts) {
+      //    $scope.contacts = allContacts;
+      //  }
+      //  console.log($cordovaSocialSharing); $scope.shareViaEmail = function(email) {
 
-      $scope.shareViaEmail = function(email) {
+      $cordovaSocialSharing.shareViaEmail(null, null, [email]);
+    }
 
-        $cordovaSocialSharing.shareViaEmail(null, null, [email]);
-      }
+    $scope.shareViaSMS = function(mobilePhone) {
 
-      $scope.shareViaSMS = function(mobilePhone) {
-
-        $cordovaSocialSharing.shareViaSMS(null, mobilePhone);
-
-      }
-
-      $scope.shareViaWhatsAppToReceiver = function(mobilePhone) {
-        $cordovaSocialSharing.shareViaWhatsAppToReceiver(mobilePhone)
-        $cordovaSocialSharing.shareViaWhatsAppToReceiver(100)
-      }
-
-      $scope.shareViaWhatsApp = function() {
-        alert('shareViaWhatsApp')
-        $cordovaSocialSharing.shareViaWhatsApp()
-      }
-
-      $scope.empPic = function(base64String) {
-        return "data:image/jpg;" + base64String;
-      }
-
-      $scope.managerInfo = {}
-
-      $scope.getTreeData = function(person) {
-        var tree = {};
-        person.orgTree.forEach(function(c) {
-
-          if (c.personId == person.directManagerNumber) {
-            $scope.managerInfo = c;
-            return false;
-          }
-          tree[c.directManagerNumber] = tree[c.directManagerNumber] || {};
-          tree[c.directManagerNumber].members = tree[c.directManagerNumber].members || [];
-
-          if (person.personId == c.directManagerNumber)
-            tree[person.personId].members.push(c)
-          else
-            tree[c.personId] = c;
-        })
-        return tree;
-
-      }
-
-
-      $scope.getContact = function() {
-        PelApi.getLocalJson("mocks/phonebook_details.json")
-          .success((data, status, headers, config) => {
-            console.log(JSON.stringify(data))
-            $scope.contact = data;
-            $scope.title = "פרטי עובד : " + data.lastName + " " + data.firstName;
-            $scope.contact.tree = $scope.getTreeData(data);
-
-
-          })
-          .error((errorStr, httpStatus, headers, config) => {})
-      }
-      $scope.getContact();
+      $cordovaSocialSharing.shareViaSMS(null, mobilePhone);
 
     }
-  ]);
+
+    $scope.shareViaWhatsAppToReceiver = function(mobilePhone) {
+      $cordovaSocialSharing.shareViaWhatsAppToReceiver(mobilePhone)
+      $cordovaSocialSharing.shareViaWhatsAppToReceiver(100)
+    }
+
+    $scope.shareViaWhatsApp = function() {
+      alert('shareViaWhatsApp')
+      $cordovaSocialSharing.shareViaWhatsApp()
+    }
+
+    $scope.empPic = function(base64String) {
+      return "data:image/jpg;" + base64String;
+    }
+
+    $scope.managerInfo = {}
+
+    $scope.getTreeData = function(person) {
+      var tree = {};
+      person.orgTree.forEach(function(c) {
+
+        if (c.personId == person.directManagerNumber) {
+          $scope.managerInfo = c;
+          return false;
+        }
+        tree[c.directManagerNumber] = tree[c.directManagerNumber] || {};
+        tree[c.directManagerNumber].members = tree[c.directManagerNumber].members || [];
+
+        if (person.personId == c.directManagerNumber)
+          tree[person.personId].members.push(c)
+        else
+          tree[c.personId] = c;
+      })
+      return tree;
+
+    }
+
+
+    $scope.getContact = function() {
+      PelApi.getLocalJson("mocks/phonebook_details.json")
+        .success((data, status, headers, config) => {
+          console.log(JSON.stringify(data))
+          $scope.contact = data;
+          $scope.title = "פרטי עובד : " + data.lastName + " " + data.firstName;
+          $scope.contact.tree = $scope.getTreeData(data);
+
+
+        })
+        .error((errorStr, httpStatus, headers, config) => {})
+    }
+    $scope.getContact();
+
+  }]);
