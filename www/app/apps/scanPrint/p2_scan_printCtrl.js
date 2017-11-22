48a6b7e5b76ac8a50997142ef81481ed30e8bb43
Golan Hadad <ghadad@gmail.com>
2017-11-22 14:33:24 +0200

    add ngCordova module to scan module

--- www/app/apps/scanPrint/p2_scan_printCtrl.js
+++ www/app/apps/scanPrint/p2_scan_printCtrl.js
@@ -1,7 +1,7 @@
 /**
  * Created by User on 12/12/2016.
  */
-angular.module('pele.prinScan', ['ngCordova'])
+angular.module('pele')
   .controller('p2_scan_printCtrl', function($scope, $stateParams, $cordovaBarcodeScanner, $ionicLoading, $ionicPopup, $localStorage, $ionicModal, PelApi) {
     $scope.modal = $ionicModal.fromTemplate('<ion-modal-view>' +
       '<ion-header-bar class="bar bar-header bar-positive">' +
