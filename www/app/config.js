c0fc653dfd8d5cdec8d786c98d7e80f9dee0a6b7
golanh <golanh@localhost.localdomain>
2018-06-24 17:44:35 +0300

    fix iphone-x cover resulotion

--- www/app/config.js
+++ www/app/config.js
@@ -176,7 +176,7 @@ angular.module('pele.config', [])
     gw_timeout: 15000,
     config: {
       contactIdPrefix: "pelephone",
-      APP_VERSION: "119",
+      APP_VERSION: "119.1",
       SCAN_PRINT_SCANNING_ERROR: "שגיאה בסריקה",
       PIN_CODE_AUTHENTICATION_REQUIRED_CODE: "10000",
       IS_TOKEN_VALID: "N",
